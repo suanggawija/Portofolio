8f338bf7d01fa5fb5bedec76b43b3f939ef6588d
suangga <suanggawija@gmail.com>
2023-04-29 02:02:27 +0800

    update responsive

--- src/component/molekul/Footer/index.jsx
+++ src/component/molekul/Footer/index.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Github, Logo } from '../../../assets'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <div className='flex w-full justify-center'>
-         <div className='flex flex-col items-center text-gray-400 pt-8 pb-28 md:py-8 border-t-2 w-3/4 text-center md:w-2/3'>
+         <div className='flex flex-col items-center text-gray-400 pt-8 pb-28 md:py-8 border-t-2 w-11/12 text-center md:w-2/3'>
             <div className="flex gap-5 items-center mb-3 ">
-                <a href=""><img src={Logo} alt="" className='h-6' /></a>
-                <a href="">Home</a>
-                <a href="">About</a>
-                <a href="">Project</a>
-                <a href="">Contact</a>
-                <a href=""><img src={Github} alt="" className='h-5' /></a>
+              <Link to={'/'}><img src={Logo} alt="" className=' hidden md:block h-6' /></Link>
+              <Link to={'/'}>Home</Link>
+              <Link to={'/about'}>About</Link>
+              <Link to={'/project'}>Project</Link>
+              <Link to={'/contact'}>Contact</Link>
+              <Link to={'https://github.com/suanggawija'}><img src={Github} alt="" className='h-5'/></Link>
             </div>
-            <span className='text-md'>@2023 Suangga Wijanatha | Dibuat menggunakan React Js + Vite</span>
+            <span className='text-sm md:text-md'>@2023 Suangga Wijanatha | Dibuat menggunakan React Js + Vite</span>
         </div>
     </div>
    
--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
     <div className='flex w-full h-screen items-center justify-center flex-col gap-3'>
         <span className='text-gray-400 text-4xl'>404 | Pages Not Found</span>
-        <p className='text-lg text-gray-400'> Kembali ke halaman <a href="/" className='text-main-color font-semibold'>Beranda</a> </p>
+        <p className='text-lg text-gray-400'> Kembali ke halaman
+          <Link to={'/'} className='text-main-color font-semibold'>Beranda</Link>
+        </p>
     </div>
   )
 }
